49e9a89b3f10435fa70de479f4d23f5b8485c199
Frank Desmettre <frank.desmettre@gmail.com>
2017-05-05 16:13:08 +0200

    Add all SIP2 request messages

--- test/message.js
+++ test/message.js
@@ -4,57 +4,24 @@ const assert = require('assert');
 
 // Messages
 const Message = require('../lib/Message');
-const Checkout = require('../lib/messages/Checkout');
-const Checkin = require('../lib/messages/Checkin');
 const Login = require('../lib/messages/Login');
+const SCStatus = require('../lib/messages/SCStatus');
+const RequestResend = require('../lib/messages/RequestResend');
 const PatronStatus = require('../lib/messages/PatronStatus');
 const PatronInformation = require('../lib/messages/PatronInformation');
 const BlockPatron = require('../lib/messages/BlockPatron');
-const FeePaid = require('../lib/messages/FeePaid');
 const PatronEnable = require('../lib/messages/PatronEnable');
 const ItemInformation = require('../lib/messages/ItemInformation');
+const Checkout = require('../lib/messages/Checkout');
+const Checkin = require('../lib/messages/Checkin');
+const FeePaid = require('../lib/messages/FeePaid');
 const EndPatronSession = require('../lib/messages/EndPatronSession');
 
 // Variables
 const FeeType = require('../lib/variables/FeeType');
 const CurrencyType = require('../lib/variables/CurrencyType');
 const PaymentType = require('../lib/variables/PaymentType');
-
-describe('Checkout', () => {
-  describe('#getMessage', () => {
-    it('should build a SIP2 checkout message', (done) => {
-      const scRenewalPolicy = true;
-      const nbDueDate = Message.getDateTime(new Date(1996, 1, 12, 10, 5, 14));
-      const itemIdentifier = '000000000005792';
-      const checkoutMessage = new Checkout(scRenewalPolicy, nbDueDate, itemIdentifier);
-      checkoutMessage.patronIdentifier = '104000000105';
-      checkoutMessage.sequence = 3;
-      checkoutMessage.transactionDate = Message.getDateTime(new Date(1996, 1, 12, 10, 5, 14));
-      const testMessage = '11YN19960212    10051419960212    100514AO|AA104000000105|AB000000000005792|AC|AY3AZEDB7\r\n';
-      assert.equal(checkoutMessage.getMessage(), testMessage);
-      done();
-    });
-  });
-});
-
-describe('Checkin', () => {
-  describe('#getMessage', () => {
-    it('should build a SIP2 checkin message', (done) => {
-      const returnDate = null;
-      const location = 'Certification Terminal Location';
-      const itemIdentifier = 'CheckInBook';
-      const checkinMessage = new Checkin(returnDate, location, itemIdentifier);
-      checkinMessage.institutionId = 'Certification Institute ID';
-      checkinMessage.terminalPassword = 'TPWord';
-      checkinMessage.sequence = 2;
-      checkinMessage.cancel = false;
-      checkinMessage.transactionDate = Message.getDateTime(new Date(1998, 7, 21, 8, 57, 21));
-      const testMessage = '09N19980821    085721                  APCertification Terminal Location|AOCertification Institute ID|ABCheckInBook|ACTPWord|BIN|AY2AZD6A5\r\n';
-      assert.equal(checkinMessage.getMessage(), testMessage);
-      done();
-    });
-  });
-});
+const StatusCode = require('../lib/variables/StatusCode');
 
 describe('Login', () => {
   describe('#getMessage', () => {
@@ -71,15 +38,43 @@ describe('Login', () => {
   });
 });
 
+describe('SCStatus', () => {
+  describe('#getMessage', () => {
+    it('should build a SIP2 SC status message', (done) => {
+      const status = StatusCode.OK;
+      const maxPrintWidth = '40';
+      const protocolVersion = '1.00';
+      const scStatusMessage = new SCStatus(status, maxPrintWidth, protocolVersion);
+      scStatusMessage.sequence = 1;
+      const testMessage = '990401.00|AY1AZFC59\r\n';
+      assert.equal(scStatusMessage.getMessage(), testMessage);
+      done();
+    });
+  });
+});
+
+describe('RequestResend', () => {
+  describe('#getMessage', () => {
+    it('should build a SIP2 request resend message', (done) => {
+      const requestResendMessage = new RequestResend();
+      requestResendMessage.sequence = 1;
+      requestResendMessage.errorDetection = false;
+      const testMessage = '97\r\n';
+      assert.equal(requestResendMessage.getMessage(), testMessage);
+      done();
+    });
+  });
+});
+
 describe('PatronStatus', () => {
   describe('#getMessage', () => {
-    it('should build a SIP2 item information message', (done) => {
+    it('should build a SIP2 patron status message', (done) => {
       const patronStatusMessage = new PatronStatus();
-      patronStatusMessage.sequence = 4;
-      patronStatusMessage.institutionId = 'Certification Institute ID';
-      patronStatusMessage.patronIdentifier = 'PatronID';
-      patronStatusMessage.transactionDate = Message.getDateTime(new Date(1998, 6, 23, 9, 42, 40));
-      const testMessage = '2300119960212    100239AOid_21|104000000105|AC|AD|AY2AZF400\r\n';
+      patronStatusMessage.sequence = 2;
+      patronStatusMessage.institutionId = 'id_21';
+      patronStatusMessage.patronIdentifier = '104000000105';
+      patronStatusMessage.transactionDate = Message.getDateTime(new Date(1996, 1, 12, 10, 2, 39));
+      const testMessage = '2300119960212    100239AOid_21|AA104000000105|AC|AD|AY2AZF271\r\n';
       assert.equal(patronStatusMessage.getMessage(), testMessage);
       done();
     });
@@ -118,30 +113,6 @@ describe('BlockPatron', () => {
   });
 });
 
-describe('FeePaid', () => {
-  describe('#getMessage', () => {
-    it('should build a SIP2 fee paid message', (done) => {
-      const feeType = FeeType.OVERDUE;
-      const paymentType = PaymentType.VISA;
-      const currencyType = CurrencyType.US_DOLLAR;
-      const feeAmount = '111.11';
-      const feeIdentifier = false;
-      const transactionId = 'TransactionID';
-      const institutionId = 'Certification Institute ID';
-      const patronIdentifier = 'PatronID';
-
-      const feeTypeMessage = new FeePaid(feeType, paymentType, currencyType, feeAmount, feeIdentifier, transactionId);
-      feeTypeMessage.sequence = 2;
-      feeTypeMessage.institutionId = institutionId;
-      feeTypeMessage.patronIdentifier = patronIdentifier;
-      feeTypeMessage.transactionDate = Message.getDateTime(new Date(1998, 6, 23, 9, 32, 11));
-      const testMessage = '3719980723    0932110401USDBV111.11|AOCertification Institute ID|AAPatronID|BKTransactionID|AY2AZE1EF\r\n';
-      assert.equal(feeTypeMessage.getMessage(), testMessage);
-      done();
-    });
-  });
-});
-
 describe('PatronEnable', () => {
   describe('#getMessage', () => {
     it('should build a SIP2 item information message', (done) => {
@@ -167,6 +138,66 @@ describe('ItemInformation', () => {
       itemInformationMessage.transactionDate = Message.getDateTime(new Date(1998, 6, 23, 10, 0, 0));
       const testMessage = '1719980723    100000AOCertification Institute ID|ABItemBook|AY1AZEBEB\r\n';
       assert.equal(itemInformationMessage.getMessage(), testMessage);
+      done();
+    });
+  });
+});
+
+describe('Checkout', () => {
+  describe('#getMessage', () => {
+    it('should build a SIP2 checkout message', (done) => {
+      const scRenewalPolicy = true;
+      const nbDueDate = Message.getDateTime(new Date(1996, 1, 12, 10, 5, 14));
+      const itemIdentifier = '000000000005792';
+      const checkoutMessage = new Checkout(scRenewalPolicy, nbDueDate, itemIdentifier);
+      checkoutMessage.patronIdentifier = '104000000105';
+      checkoutMessage.sequence = 3;
+      checkoutMessage.transactionDate = Message.getDateTime(new Date(1996, 1, 12, 10, 5, 14));
+      const testMessage = '11YN19960212    10051419960212    100514AO|AA104000000105|AB000000000005792|AC|AY3AZEDC2\r\n';
+      assert.equal(checkoutMessage.getMessage(), testMessage);
+      done();
+    });
+  });
+});
+
+describe('Checkin', () => {
+  describe('#getMessage', () => {
+    it('should build a SIP2 checkin message', (done) => {
+      const returnDate = null;
+      const location = 'Certification Terminal Location';
+      const itemIdentifier = 'CheckInBook';
+      const checkinMessage = new Checkin(returnDate, location, itemIdentifier);
+      checkinMessage.institutionId = 'Certification Institute ID';
+      checkinMessage.terminalPassword = 'TPWord';
+      checkinMessage.sequence = 2;
+      checkinMessage.cancel = false;
+      checkinMessage.transactionDate = Message.getDateTime(new Date(1998, 7, 21, 8, 57, 21));
+      const testMessage = '09N19980821    085721                  APCertification Terminal Location|AOCertification Institute ID|ABCheckInBook|ACTPWord|BIN|AY2AZD6A5\r\n';
+      assert.equal(checkinMessage.getMessage(), testMessage);
+      done();
+    });
+  });
+});
+
+describe('FeePaid', () => {
+  describe('#getMessage', () => {
+    it('should build a SIP2 fee paid message', (done) => {
+      const feeType = FeeType.OVERDUE;
+      const paymentType = PaymentType.VISA;
+      const currencyType = CurrencyType.US_DOLLAR;
+      const feeAmount = '111.11';
+      const feeIdentifier = false;
+      const transactionId = 'TransactionID';
+      const institutionId = 'Certification Institute ID';
+      const patronIdentifier = 'PatronID';
+
+      const feeTypeMessage = new FeePaid(feeType, paymentType, currencyType, feeAmount, feeIdentifier, transactionId);
+      feeTypeMessage.sequence = 2;
+      feeTypeMessage.institutionId = institutionId;
+      feeTypeMessage.patronIdentifier = patronIdentifier;
+      feeTypeMessage.transactionDate = Message.getDateTime(new Date(1998, 6, 23, 9, 32, 11));
+      const testMessage = '3719980723    0932110401USDBV111.11|AOCertification Institute ID|AAPatronID|BKTransactionID|AY2AZE1EF\r\n';
+      assert.equal(feeTypeMessage.getMessage(), testMessage);
       done();
     });
   });
