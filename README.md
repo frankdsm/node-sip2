d4ba2e73bfa28818b566040d99e1402a815a9f50
Frank Desmettre <frank.desmettre@gmail.com>
2017-05-29 17:19:35 +0200

    Export requests/responses/connection

--- README.md
+++ README.md
@@ -7,17 +7,18 @@ $ npm install sip2
 ## Usage
 ```
 const async = require('async');
-const sip2 = new SIP2(host, port);
+const SIP2 = require('sip2');
+const sip2connection = new SIP2.Connection(host, port);
 
 function connect(cb) {
-  sip2.connect(cb);
+  sip2connection.connect(cb);
 }
 
 function requestLogin(cb) {
   // Login request
-  const loginRequest = new Login(username, password);
+  const loginRequest = new SIP2.LoginRequest(username, password);
   loginRequest.sequence = 1;
-  sip2.send(loginRequest.getMessage(), cb);
+  sip2connection.send(loginRequest.getMessage(), cb);
 }
 
 function requestPatronInformation(loginResponse, cb) {
@@ -26,11 +27,11 @@ function requestPatronInformation(loginResponse, cb) {
   }
   // Patron information request
   const type = 'hold';
-  const patronInformationRequest = new PatronInformation(type, 1, 2);
+  const patronInformationRequest = new SIP2.PatronInformationRequest(type, 1, 2);
   patronInformationRequest.sequence = 2;
   patronInformationRequest.institutionId = 'OBA';
   patronInformationRequest.patronIdentifier = '900000001025';
-  sip2.send(patronInformationRequest.getMessage(), cb);
+  sip2connection.send(patronInformationRequest.getMessage(), cb);
 }
 
 async.waterfall([
--- index.js
+++ index.js
@@ -1,5 +1,55 @@
 'use strict';
 
-const SIP2 = require('./lib/sip2');
+const Connection = require('./lib/Connection');
 
-module.exports = SIP2;
+// Requests
+const LoginRequest = require('./lib/requests/Login');
+const SCStatusRequest = require('./lib/requests/SCStatus');
+const RequestResendRequest = require('./lib/requests/RequestResend');
+const PatronStatusRequest = require('./lib/requests/PatronStatus');
+const PatronInformationRequest = require('./lib/requests/PatronInformation');
+const BlockPatronRequest = require('./lib/requests/BlockPatron');
+const PatronEnableRequest = require('./lib/requests/PatronEnable');
+const ItemInformationRequest = require('./lib/requests/ItemInformation');
+const CheckoutRequest = require('./lib/requests/Checkout');
+const CheckinRequest = require('./lib/requests/Checkin');
+const FeePaidRequest = require('./lib/requests/FeePaid');
+const EndPatronSessionRequest = require('./lib/requests/EndPatronSession');
+
+// Responses
+const CheckinResponse = require('./lib/responses/Checkin');
+const CheckoutResponse = require('./lib/responses/Checkout');
+const EndSessionResponse = require('./lib/responses/EndSession');
+const FeePaidResponse = require('./lib/responses/FeePaid');
+const ItemInformationResponse = require('./lib/responses/ItemInformation');
+const LoginResponse = require('./lib/responses/Login');
+const PatronEnableResponse = require('./lib/responses/PatronEnable');
+const PatronInformationResponse = require('./lib/responses/PatronInformation');
+const PatronStatusResponse = require('./lib/responses/PatronStatus');
+const ACStatusResponse = require('./lib/responses/ACStatus');
+
+module.exports = {
+  Connection,
+  LoginRequest,
+  SCStatusRequest,
+  RequestResendRequest,
+  PatronStatusRequest,
+  PatronInformationRequest,
+  BlockPatronRequest,
+  PatronEnableRequest,
+  ItemInformationRequest,
+  CheckoutRequest,
+  CheckinRequest,
+  FeePaidRequest,
+  EndPatronSessionRequest,
+  CheckinResponse,
+  CheckoutResponse,
+  EndSessionResponse,
+  FeePaidResponse,
+  ItemInformationResponse,
+  LoginResponse,
+  PatronEnableResponse,
+  PatronInformationResponse,
+  PatronStatusResponse,
+  ACStatusResponse,
+};
